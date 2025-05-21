dbc6261b7b9e20ec91499b2182de76fc9446a9c7
Platypus-Albert <heiliosgp.sih@gmail.com>
2025-05-21 16:48:14 +0800

    [UPDATE] TODO PROJECT 2-1 back
    Back button in Scoreboard!

--- Scene/ScoreboardScene.cpp
+++ Scene/ScoreboardScene.cpp
@@ -20,6 +20,12 @@ void ScoreboardScene::Initialize() {
     int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
     int halfW = w / 2;
     int halfH = h / 2;
+    Engine::ImageButton *btn;
+
+    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 200, halfH * 3 / 2 - 50, 400, 100);
+    btn->SetOnClickCallback(std::bind(&ScoreboardScene::BackOnClick, this));
+    AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("Back", "pirulen.ttf", 48, halfW, halfH * 3 / 2, 0, 0, 0, 255, 0.5, 0.5));
 }
 
 void ScoreboardScene::Terminate() {
@@ -27,5 +33,5 @@ void ScoreboardScene::Terminate() {
 }
 
 void ScoreboardScene::BackOnClick() {
-    Engine::GameEngine::GetInstance().ChangeScene("start");
+    Engine::GameEngine::GetInstance().ChangeScene("stage-select");
 }
